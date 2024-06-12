513799802465e000f79e69fd028cec847abba951
Nabdeep37 <123183458+Nabdeep37@users.noreply.github.com>
2024-06-11 20:09:27 -0700

    Add files via upload

---
+++ SpringGuessApplication.java
@@ -0,0 +1,13 @@
+package com.example.Spring_guess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGuessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGuessApplication.class, args);
+	}
+
+}
